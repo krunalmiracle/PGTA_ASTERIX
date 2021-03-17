2f6fea6651aec63ee6d6ed1f624e5e727fa274f9
Maite-Fernandez <61283848+Maite-Fernandez@users.noreply.github.com>
2021-03-17 19:13:23 +0100

    File reader

--- Libraries/CAT10.cs
+++ Libraries/CAT10.cs
@@ -8,5 +8,33 @@ namespace DecerixUPC.Libraries
 {
     class CAT10
     {
+        public string[] message;
+        public string FSPEC1;
+        public string CAT = "10";
+        public string LEN;
+        public int Id;
+        public int CAT10Id;
+        public int airportId;
+
+        public Dictionary<char, string> HexToBinary = new Dictionary<char, string>
+        {
+            { '0', "0000" },
+            { '1', "0001" },
+            { '2', "0010" },
+            { '3', "0011" },
+            { '4', "0100" },
+            { '5', "0101" },
+            { '6', "0110" },
+            { '7', "0111" },
+            { '8', "1000" },
+            { '9', "1001" },
+            { 'A', "1010" },
+            { 'B', "1011" },
+            { 'C', "1100" },
+            { 'D', "1101" },
+            { 'E', "1110" },
+            { 'F', "1111" }
+        };
+       
     }
 }
---
+++ Libraries/File.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data;
+
+
+namespace LibreriaClases
+{
+    public class File
+    {
+        string path;
+        List<CAT10> listCAT10 = new List<CAT10>();
+        List<CAT21> listCAT21 = new List<CAT21>();
+        DataTable tableCAT10 = new DataTable();
+        DataTable tableCAT21 = new DataTable();
+
+
+        public Fichero(string name)
+        {
+            this.path = name;
+        }
+
+        public List<CAT10> getListCAT10()
+        {
+            return listCAT10;
+        }
+        public List<CAT21> getListCAT21()
+        {
+            return listCAT21;
+        }
+
+        public void read()
+        {
+            byte[] fileBytes = File.ReadAllBytes(path);
+            List<byte[]> listbyte = new List<byte[]>();
+            int i = 0;
+            int count = fileBytes[2];
+
+            while (i < fileBytes.Length)
+            {
+                byte[] array = new byte[count];
+                for (int j = 0; j < array.Length; j++)
+                {
+                    array[j] = fileBytes[i];
+                    i++;
+                }
+                listbyte.Add(array);
+                if (i + 2 < fileBytes.Length)
+                {
+                    count = fileBytes[i + 2];
+                }
+            }
+
+            List<string[]> listhex = new List<string[]>();
+            for (int x = 0; x < listbyte.Count; x++)
+            {
+                byte[] buffer = listbyte[x];
+                string[] arrayhex = new string[buffer.Length];
+                for (int y = 0; y < buffer.Length; y++)
+                {
+                    arrayhex[y] = buffer[y].ToString("X");
+                }
+                listahex.Add(arrayhex);
+            }
+
+
+            for (int q = 0; q < listhex.Count; q++)
+            {
+                string[] arraystring = listahex[q];
+                int CAT = int.Parse(arraystring[0], System.Globalization.NumberStyles.HexNumber);
+
+                if (CAT == 10)
+                {
+                    CAT10 newcat10 = new CAT10(arraystring);
+                    listCAT10.Add(newcat10);
+                }
+                else if (CAT == 21)
+                {
+                    CAT21 newcat21 = new CAT21(arraystring);
+                    listCAT21.Add(newcat21);
+                }
+            }
+            
+
+        }
+
+        public DataTable getTableCAT10()
+        {
+            return tableCAT10;
+        }
+        public DataTable getTableCAT21()
+        {
+            return tableCAT21;
+        }
+    }
+}
